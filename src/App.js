7c6456bc80a00199d45a5f22f5371c3935592bc6
Ahngyuho <gyuho0897@gmail.com>
2022-04-07 22:47:01 +0900

    2022 04 07 22:46

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//import Button from "./Button";
+//import styles from "./App.module.css";
+import { useState , useEffect } from "react";
+import  Movie from "./components/Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail"
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+//이제 App()는 router를 render하게 됨
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+    <Switch>
+      <Route path = "/movie/:id"> 
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
